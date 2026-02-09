acd101d3a91681efd55c9ecaa22518d19d7be4e7
pedro piccinin soster <pedrososter2019@gmail.com>
2026-02-09 00:08:21 -0300

    Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="align-items:center justify-center flex mt-10 text-5xl mb-45">
+    <img src="parchment.png" alt="pergamin"> <h1>CLICKER GAME</h1>
+</div>
+
+<div class="flex align-items:center justify-center pr-30 pl-30 text-9xl justify-self-center border-5 rounded-2xl shadow-2xl shadow-blue-800  bg-blue-800">
+    <button class=" border-4 rounded-2xl w-200 border-r-8 border-l-8 h-45 bg-blue-600 shadow-2xl shadow-blue-600"> COMEÇAR </button>
+</div>

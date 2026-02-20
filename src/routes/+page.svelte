228396ed47517555d29db44747e2e7378f067dd5
Pedro <pedrososter2019@gmail.com>
2026-02-20 18:25:40 -0300

    inital design for clicking page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <div class="align-items:center justify-center flex mt-10 text-5xl mb-45">
-    <img src="parchment.png" alt="pergamin"> <h1>CLICKER GAME</h1>
+    
+    <h1 class="z-1 mt-20 mr-3">CLICKER GAME</h1>
+    <img class="absolute" src="parchment.png" alt="pergamin"> 
 </div>
 
-<div class="flex align-items:center justify-center pr-30 pl-30 text-9xl justify-self-center border-5 rounded-2xl shadow-2xl shadow-blue-800  bg-blue-800">
-    <button class=" border-4 rounded-2xl w-200 border-r-8 border-l-8 h-45 bg-blue-600 shadow-2xl shadow-blue-600"> COMEÇAR </button>
+
+<div class="flex align-items:center justify-center pr-30 pl-30 text-9xl justify-self-center border-5 rounded-2xl shadow-2xl shadow-blue-800  bg-blue-800 font-serif">
+    <button class=" border-4 rounded-2xl w-200 border-r-8 border-l-8 h-45 bg-blue-600 shadow-2xl shadow-blue-600 font-serif"> 
+     <a href="/Main">COMEÇAR</a>
+    </button>
 </div>
---
+++ src/routes/Main/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    /** @type {import('./$types').PageProps} */
+    let { data } = $props();
+
+    let pressed = $state(false);
+    let clicks = $state(0);
+
+    function press(){
+        pressed = true;
+        clicks++;
+         setTimeout(()=> pressed = false, 150
+        );
+    }
+
+</script>
+
+<div class="justify-center items-center h-screen flex">
+    <div class="absolute self-center justify-center right-99/100 bottom-95/100">{clicks}</div>
+
+    <div class="bg-gray-600 w-75 h-75 rounded-full mt-7"></div>
+    <div class="bg-red-800 w-60 h-60 rounded-full mt-7 absolute"></div>
+    <button
+        class="bitcount-grid-double-texto-botao absolute mb-5 font-sans self-center text-6xl flex rounded-full bg-red-600 w-60 h-60 justify-center items-center text-center"
+        class:pressed={pressed}
+        onclick={press}
+    >
+        CLICK
+</button>
+</div>
